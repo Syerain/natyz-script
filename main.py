394137191e47e0697a050385d00d3ce9e71583fb
Syerain <zenraeno@gmail.com>
2026-09-20 21:34:46 +0800

    update

--- main.py
+++ main.py
@@ -15,6 +15,8 @@ fbcount = 0
 
 ziplist = []
 fblist = []
+errfblist = []
+badfblist = []
 
 
 for zp in Path(".").glob("*.zip"):
@@ -26,14 +28,14 @@ for zp in Path(".").glob("*.zip"):
 print("\t(" + str(len(ziplist)) + ") " + "zip pkgs founded")
 print("\t(" + str(len(fblist)) + ") " + "FB pkgs detected")
 
+print("proc pkg layout analyse & transfer ...")
 for zip in fblist:
     # check formats
-    print("proc pkg layout analyse & transfer ...")
     is_zdlf = False
     ## std ZDLF
     try:
         with zipfile.ZipFile(zip, 'r') as zf:
-            dir = Path("xt") / zip.stem
+            dirxt = Path("xt") / zip.stem
             #zf.extractall(dir)
             names = zf.namelist()
 
@@ -61,48 +63,68 @@ for zip in fblist:
                 is_zdlf = True
 
     except Exception as e:
-        print("\t[!] failed to analyse pkg as ZDLF layout: " + zp.name)
+        print("\t[!] failed in layout analysation: " + zp.name)
+        errfblist.append(zp)
 
     if is_zdlf:
-        print(zp.stem + "\non process:")
+        print("\t" + zp.stem)
             
         # 1-1 xt/ -> dir of extract output
-        dir = Path("xt") / zp.stem
-        dir.mkdir(parents=True, exist_ok=True)
+     
+        dirxtfb = dirxt / top
             
         # 解压
         with zipfile.ZipFile(zp) as z:
-            z.extractall(dir)
-        print("\tok extract")
-        
-          # 目录
-          # [!] notice the spell-sensitive feature on systems like linux
-        src_dir_zh = next(dir.glob("*/chinese"))
-        src_dir_en = next(dir.glob("*/english"))
+            z.extractall(dirxt)
+        print("\t\tok extract")
+
+        # dir paths
         dst_dir_zh = Path(".") / "out" / "zh" / zp.stem
         dst_dir_zh.mkdir(parents=True, exist_ok=True)
         dst_dir_en = Path(".") / "out" / "en" / zp.stem
         dst_dir_en.mkdir(parents=True, exist_ok=True)
+        dst_dir_line = Path(".") / "out" / "line" / zp.stem
+        dst_dir_line.mkdir(parents=True, exist_ok=True)
         
         # 遍历
         zhcount = 0
         encount = 0
+        linecount = 0
             
-        ## zh
-        for f in src_dir_zh.iterdir():
-            if f.is_file():
-                shutil.move(str(f), str(dst_dir_zh / f.name))
-                zhcount = zhcount + 1
-        print("\tproc: " + str(zhcount) + " img transfered")
-        print("\tok zh transfer")
-            
-        ## en
-        for f in src_dir_en.iterdir():
-            if f.is_file():
-                shutil.move(str(f), str(dst_dir_en / f.name))
-                encount = encount + 1            
-        print("\tproc: " + str(encount) + " img transfered")
-        print("\tok en transfer")
-        
-        # 收尾
-        print("\tok process")
+        # transfer
+        # [!] notice the spell-sensitive feature on systems like linux
+        if os.path.exists(dirxtfb / "chinese"):
+            src_dir_zh = dirxtfb / "chinese"
+            for f in src_dir_zh.iterdir():
+                if f.is_file():
+                    shutil.move(str(f), str(dst_dir_zh / f.name))
+                    zhcount = zhcount + 1
+            print("\t\tok zh transfer" + " (" + str(zhcount) + ") obj")
+
+        if os.path.exists(dirxtfb / "english"):
+            src_dir_en = dirxtfb / "english"
+            for f in src_dir_en.iterdir():
+                if f.is_file():
+                    shutil.move(str(f), str(dst_dir_en / f.name))
+                    encount = encount + 1
+            print("\t\tok en transfer" + " (" + str(encount) + ") obj")
+
+        if os.path.exists(dirxtfb / "lineart"):
+            src_dir_line = dirxtfb / "lineart"
+            for f in src_dir_line.iterdir():
+                if f.is_file():
+                    shutil.move(str(f), str(dst_dir_line / f.name))
+                    linecount = linecount + 1
+            print("\t\tok line transfer" + " (" + str(linecount) + ") obj")
+
+        # end
+        shutil.rmtree(dirxt)
+        print("\t\tok xtfb dir cleaned")
+        print("\t\tok process")
+
+    else: 
+        badfblist.append(zip)
+
+print("bad layout FB pkgs:")
+for bad in badfblist:
+    print("\t" + bad.stem)
